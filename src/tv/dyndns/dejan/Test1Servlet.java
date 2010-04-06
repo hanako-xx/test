68e38c54e5c1a0b0642159cc0012a9636b648223
Hanako <hanako2005@gmail.com>
2010-04-07 01:40:12 +0900

    1

---
+++ src/tv/dyndns/dejan/Test1Servlet.java
@@ -0,0 +1,11 @@
+package tv.dyndns.dejan;
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Test1Servlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
