083909d85634e8775659673425696ae245edc91e
Hanako <hanako2005@gmail.com>
2010-04-10 17:38:14 +0900

    2

--- src/tv/dyndns/dejan/Test1Servlet.java
+++ src/tv/dyndns/dejan/Test1Servlet.java
@@ -7,5 +7,6 @@ public class Test1Servlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/plain");
 		resp.getWriter().println("Hello, world");
+		
 	}
 }
